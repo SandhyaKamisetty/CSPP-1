1cc29023b43b91ebec38fa8ff2e3138702e94472
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-02 22:20:06 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-assignments/m4/p2/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/p2/bob_counter.py
@@ -7,27 +7,26 @@ Number of times bob occurs is: 2
 '''
 
 def main():
-	'''
-	@author : SandhyaKamisetty
-	To print number of times the string 'bob' occurs in s.
-	'''
-	n_m = input()
-	s_m = "bob"
-	c_m = 0
-	for i_m in range(0, len(n_m) - 2):
-		j_m = 0
-		k_m = i_m
-		v_m = 0
-		while(j_m < 3 and n_m[k_m] == s_m[j_m]):
-			v_m += 1
-			k_m += 1
-			j_m += 1
-	    if v_m == 3:
-	    	c_m += 1
-	print(c_m)
-	# the input string is in s
-	# remove pass and start your code here
-	pass
+    '''
+    @author : SandhyaKamisetty
+    To print number of times the string 'bob' occurs in s.
+    '''
+    n_m = input()
+    s_m = "bob"
+    c_m = 0
+    for i_m in range(0, len(n_m) - 2):
+        j_m = 0
+        k_m = i_m
+        v_m = 0
+        while(j_m < 3 and n_m[k_m] == s_m[j_m]):
+            v_m += 1
+            k_m += 1
+            j_m += 1
+            if v_m == 3:
+                c_m += 1
+    print(c_m)
+    # the input string is in s
+    # remove pass and start your code here
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
