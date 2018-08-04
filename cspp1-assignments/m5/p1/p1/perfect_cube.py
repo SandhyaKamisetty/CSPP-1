953ae83f8d55def1aaf7b7b31bf9e149f4be205d
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-04 17:59:05 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- cspp1-assignments/m5/p1/p1/perfect_cube.py
+++ cspp1-assignments/m5/p1/p1/perfect_cube.py
@@ -27,9 +27,9 @@ def main():
         else:
             g_n += i_n
     if abs(g_n**3 - cube_num) >= e_n:
-        print(str(cube_num) + "is not a perfect cube")
+        print(str(cube_num) + " is not a perfect cube")
     else:
-        print(str(cube_num) + "is a perfect cube")
+        print(str(cube_num) + " is a perfect cube")
     # input is captured in s
     # watch out for the data type of value stored in s
     # your code starts here
