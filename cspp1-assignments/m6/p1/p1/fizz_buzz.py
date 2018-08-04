50a6e1b19a341d890b7e007dd5d143284ac75938
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-04 12:13:40 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/p1/p1/fizz_buzz.py
+++ cspp1-assignments/m6/p1/p1/fizz_buzz.py
@@ -15,12 +15,12 @@ def main():
     '''
     n_n = int(input())
     for i in range(1, n_n+1):
-        if i%3 == 0:
+        if i%3 == 0 and i%5 == 0:
+            print("Fizz")
+            print("Buzz")
+        elif i%3 == 0:
             print("Fizz")
         elif i%5 == 0:
-            print("Buzz")
-        elif i%3 == 0 and i%5 == 0:
-            print("Fizz")
             print("Buzz")
         else:
             print(i)
