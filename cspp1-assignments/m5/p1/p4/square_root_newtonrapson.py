cff2cb11b962e325b4907602e66401a55aa80d51
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-04 17:40:33 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

--- cspp1-assignments/m5/p1/p4/square_root_newtonrapson.py
+++ cspp1-assignments/m5/p1/p4/square_root_newtonrapson.py
@@ -11,19 +11,19 @@
 # output: 6.999999999999991
 '''
 def main():
-    '''
-    @author : SandhyaKamisetty
-    Write a python program to find the square root of the given number
-    '''
-    x_i = int(input())
-    epsilon = 0.01
-    guess = x_i/2.0
-    while abs(guess*guess - x_i) >= epsilon:
-        guess = guess - (((guess**2) - x_i)/(2*guess))
-    print(str(guess))
-    # epsilon and step are initialized
-    # don't change these values
-    # your code starts here
+	'''
+	@author : SandhyaKamisetty
+	Write a python program to find the square root of the given number
+	'''
+	x_i = int(input())
+	epsilon = 0.01
+	guess = x_i/2.0
+	while abs(guess*guess - x_i) >= epsilon:
+		guess = guess - (((guess**2) - x_i)/(2*guess))
+	print(str(guess))
+	# epsilon and step are initialized
+	# don't change these values
+	# your code starts here
 
-if __name__ == "__main__":
-    main()
+if __name__== "__main__":
+	main()
