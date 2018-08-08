491240efc704c85dcc14eae54883bf4257ee6a44
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-08 18:10:04 +0530

    " -> 0 of 9 passed. pylint: 9.44/10 "

--- cspp1-assignments/m9/p2/p2/assignment2.py
+++ cspp1-assignments/m9/p2/p2/assignment2.py
@@ -15,7 +15,7 @@ def get_guessed_word(secret_word, letters_guessed):
     # FILL IN YOUR CODE HERE...
     for i in secret_word:
         if i not in letters_guessed:
-            return _.join(i)
+            return '_' .join(secret_word)
     return i
 
 
