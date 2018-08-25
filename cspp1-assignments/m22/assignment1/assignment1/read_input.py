875898b7a9d53f882b908e859ec61e76af75c157
SandhyaKamisetty <sandhya.sam18@msitprogram.net>
2018-08-25 11:44:41 +0530

    " -> 0 of 4 passed. pylint: 9.41/10 "

--- cspp1-assignments/m22/assignment1/assignment1/read_input.py
+++ cspp1-assignments/m22/assignment1/assignment1/read_input.py
@@ -3,7 +3,9 @@ Write a python program to read multiple lines of text input and store the input 
 '''
 
 def main():
-    pass
+    no_of_lines = int(input())
+    for i in range(no_of_lines):
+    	
 
 if __name__ == '__main__':
     main()
--- cspp1-assignments/m22/assignment3/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/assignment3/tokenize.py
@@ -3,11 +3,29 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 
-def tokenize(string):
-    pass
+def tokenize(string_input):
+    '''
+    string frequency
+    '''
+    dictionary = {}
+    for word in string_input:
+        if word not in dictionary:
+            dictionary[word] = [string_input.count(word)]
+        else:
+            if (string_input.count(word)) not in dictionary[word]:
+                dictionary[word].append(string_input(word))
+    return dictionary
             
 def main():
-    pass
+    '''
+    main function
+    '''
+    string_input = []
+    lines = int(input())
+    for i in range(lines):
+        string_input.append(input())
+        i += 1
+    print(tokenize(string_input))
 
 if __name__ == '__main__':
     main()
